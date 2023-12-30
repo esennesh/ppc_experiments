b7baf3ae81a96d67bed39ce1304372543151c89f
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2023-12-29 21:40:37 -0500

    base.base_model.DensityKernel: base class for density kernels as Distributions
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- base/base_model.py
+++ base/base_model.py
@@ -3,7 +3,6 @@ import torch
 import numpy as np
 from abc import abstractmethod
 
-
 class BaseModel(pyro.nn.PyroModule):
     """
     Base class for all models
@@ -34,3 +33,24 @@ class BaseModel(pyro.nn.PyroModule):
         resume_path = str(resume_path)
         checkpoint = torch.load(resume_path)
         self.load_state_dict(checkpoint['state_dict'])
+
+class DensityKernel(BaseModel, pyro.distributions.Distribution):
+    def __init__(self):
+        self.has_enumerate_support = self.has_enumerate_support
+        self.has_rsample = self.density.has_rsample
+
+    @property
+    @abstractmethod
+    def density(self):
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def name(self):
+        raise NotImplementedError
+
+    def log_prob(self, x, *args, **kwargs):
+        return self.density.log_prob(x, *args, **kwargs)
+
+    def sample(self, *args, **kwargs):
+        return self.density.sample(*args, **kwargs)
